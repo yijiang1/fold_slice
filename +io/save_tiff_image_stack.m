cc7d73bbd6b7d968b4b2c6bc2664eeadf5c21cdf
yijiang1 <yj245@cornell.edu>
2023-10-10 10:53:24 -0500

    allow user to save tiff stack in color

--- +io/save_tiff_image_stack.m
+++ +io/save_tiff_image_stack.m
@@ -10,6 +10,7 @@ if ~isfield(par,'axis'); par.axis = 3; end
 if ~isfield(par,'range'); par.range = [0,0]; end
 if ~isfield(par,'norm_single_slice'); par.norm_single_slice = false; end
 if ~isfield(par,'name'); par.name = 'image_stack.tiff'; end
+if ~isfield(par,'colormap'); par.colormap = gray(256); end
 
 if any(par.range)
     range = par.range;
@@ -20,41 +21,41 @@ end
 switch par.axis
     case 3
         if par.norm_single_slice
-            imwrite(mat2gray(input(:,:,1)),par.name,'tiff')
+            imwrite(mat2gray(input(:,:,1))*256, par.colormap, par.name, 'tiff')
         else
-            imwrite(mat2gray(input(:,:,1),range),par.name,'tiff')
+            imwrite(mat2gray(input(:,:,1), range)*256, par.colormap, par.name, 'tiff')
         end
         for i=2:size(input,3)
             if par.norm_single_slice
-                imwrite(mat2gray(input(:,:,i)),par.name,'tiff','WriteMode','append')
+                imwrite(mat2gray(input(:,:,i))*256, par.colormap, par.name, 'tiff', 'WriteMode', 'append')
             else
-                imwrite(mat2gray(input(:,:,i),range),par.name,'tiff','WriteMode','append')
+                imwrite(mat2gray(input(:,:,i), range)*256, par.colormap, par.name,'tiff', 'WriteMode', 'append')
             end
         end
     case 2
         if par.norm_single_slice
-            imwrite(mat2gray(squeeze(input(:,1,:))),par.name,'tiff')
+            imwrite(mat2gray(squeeze(input(:,1,:)))*256, par.colormap, par.name, 'tiff')
         else
-            imwrite(mat2gray(squeeze(input(:,1,:)),range),par.name,'tiff')
+            imwrite(mat2gray(squeeze(input(:,1,:))*256, range), par.colormap, par.name, 'tiff')
         end
         for i=2:size(input,2)
             if par.norm_single_slice
-                imwrite(mat2gray(squeeze(input(:,i,:))),par.name,'tiff','WriteMode','append')
+                imwrite(mat2gray(squeeze(input(:,i,:)))*256, par.name, 'tiff', 'WriteMode', 'append')
             else
-                imwrite(mat2gray(squeeze(input(:,i,:)),range),par.name,'tiff','WriteMode','append')
+                imwrite(mat2gray(squeeze(input(:,i,:)), range)*256, par.name, 'tiff', 'WriteMode', 'append')
             end
         end
     case 1
         if par.norm_single_slice
-            imwrite(mat2gray(squeeze(input(1,:,:))),par.name,'tiff')
+            imwrite(mat2gray(squeeze(input(1,:,:)))*256, par.name, 'tiff')
         else
-            imwrite(mat2gray(squeeze(input(1,:,:)),range),par.name,'tiff')
+            imwrite(mat2gray(squeeze(input(1,:,:)), range)*256, par.name, 'tiff')
         end
         for i=2:size(input,1)
             if par.norm_single_slice
-                imwrite(mat2gray(squeeze(input(i,:,:))),par.name,'tiff','WriteMode','append')
+                imwrite(mat2gray(squeeze(input(i,:,:)))*256, par.name, 'tiff', 'WriteMode', 'append')
             else
-                imwrite(mat2gray(squeeze(input(i,:,:)),range),par.name,'tiff','WriteMode','append')
+                imwrite(mat2gray(squeeze(input(i,:,:)), range)*256, par.name, 'tiff', 'WriteMode', 'append')
             end
         end
     otherwise
@@ -62,4 +63,3 @@ switch par.axis
 end
 
 end
-
